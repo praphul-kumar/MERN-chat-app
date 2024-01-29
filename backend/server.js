9ec229306899fbb807c5c80ab5ea68ef5fd78296
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-29 16:59:35 +0530

    Added: Env file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000, console.log('Server started on PORT: 5000'))
+app.listen(PORT, console.log(`Server started on PORT: ${PORT}`))
