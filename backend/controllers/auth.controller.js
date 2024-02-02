6c1f246a6c965eb739c27461238640cc9a7f12ea
Praphul Kumar <praphulkumar2023@gmail.com>
2024-02-02 12:01:27 +0530

    Refctored: Encryption/Decryption methods

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,4 @@
-const asyncHandler = require('express-async-handler');
-const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/UserModel');
 const { signUpSchema, signInSchema } = require('../helpers/validationSchema');
 const { errorHandler } = require('../utils/error.js');
 const UserService = require('../services/user.service');
@@ -10,27 +7,23 @@ exports.login = async (req, res, next) => {
   try {
     const validationResult = await signInSchema.validateAsync(req.body);
 
-    const user = await UserService.findOne({email: validationResult.email});
+    const user = await UserService.findOne({ email: validationResult.email });
 
-    if (user) {
-      if (await bcryptjs.compare(validationResult.password, user.password)) {
-        const access_token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+    if (user && user.comparePassword(validationResult.password)) {
+      const access_token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-        const { password: pass, ...userWithoutPass } = user._doc;
+      const { password: pass, ...userWithoutPass } = user._doc;
 
-        res.cookie('access_token', access_token, { httpOnly: true })
-          .status(200).json({
-            success: true,
-            message: "User Login Successfully!!",
-            user: userWithoutPass
-          });
-      } else {
-        throw errorHandler(401, "Invalid credentials.");
-      }
+      res.cookie('access_token', access_token, { httpOnly: true })
+        .status(200).json({
+          success: true,
+          message: "User Login Successfully!!",
+          user: userWithoutPass
+        });
     } else {
-      throw errorHandler(410, 'User not Found.');
+      throw errorHandler(401, "Invalid credentials.");
     }
-  } catch(error) {
+  } catch (error) {
     if (error.isJoi === true) error.statusCode = 422;
     next(error);
   }
@@ -44,12 +37,10 @@ exports.signUp = async (req, res, next) => {
     if (doesExist) {
       throw errorHandler(409, 'Email Id is already registered.');
     } else {
-      const hashedPassword = bcryptjs.hashSync(validationResult.password, 10);
-
       const createdUser = await UserService.saveUser({
         name: validationResult.name,
         email: validationResult.email,
-        password: hashedPassword
+        password: validationResult.password
       });
 
       if (createdUser) {
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, trim: true, required: true },
@@ -7,6 +8,21 @@ const userSchema = new mongoose.Schema({
   avatar: { type: String, required: true, default: "https://i.pravatar.cc/150" },
   status: {type: Number, default: 1, required: true}
 }, { timestamps: true });
+
+// Encrypt password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt(10); // Adjust salt rounds as needed
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+// Create a method for authenticating users
+userSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
